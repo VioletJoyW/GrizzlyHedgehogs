77da61b4219cac27346c23fb3534d0d5ae84c15b
F-Towel <britton.tourel@gmail.com>
2023-12-10 03:51:40 -0500

    Finished Intro setup/decor
    Note: The last thing to do here is set the intro scripts, and connect all the levels.

--- Grizzly Hedgehogs/Assets/Scripts/ThrowawayScripts/IntroScript.cs
+++ Grizzly Hedgehogs/Assets/Scripts/ThrowawayScripts/IntroScript.cs
@@ -10,6 +10,7 @@ public class IntroScript : MonoBehaviour, ISceneScript
 	{
 		Camera.main.clearFlags = CameraClearFlags.Skybox;
 		gameManager.instance.player.transform.localScale = new Vector3(gameManager.instance.player.transform.localScale.x, 2.0f, gameManager.instance.player.transform.localScale.y);
+		Camera.main.farClipPlane = 150.0f;
 	}
 
 	public void Run()
@@ -19,6 +20,7 @@ public class IntroScript : MonoBehaviour, ISceneScript
 
 	public void Close()
 	{
+		Camera.main.farClipPlane = 50.0f;
 		Camera.main.clearFlags = CameraClearFlags.SolidColor;
 		gameManager.instance.player.transform.localScale = new Vector3(gameManager.instance.player.transform.localScale.x, 1.5f, gameManager.instance.player.transform.localScale.y);
 	}
